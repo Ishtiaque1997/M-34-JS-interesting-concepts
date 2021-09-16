4196c6e4a635a429bb5db74e02c34b13b9dcc616
Ishtiaque1997 <ishtiaquea695@gmail.com>
2021-09-16 10:26:18 +0600

    little more

--- data-types.js
+++ data-types.js
@@ -1,4 +1,5 @@
+//to find data type use type of in the right side
 const a=19;
 console.log(typeof a)
-const b='Jamal Uddin Hasan';
+const b='Rehnuma Ahmed';
 console.log(typeof b);
---
+++ primitive.js
@@ -0,0 +1,26 @@
+/*
+seven types of data type
+//primitive data types(primitive means basic and they are immutable)
+Number
+String
+Boolean
+undefined
+null
+
+symbol
+*/
+// 
+//object is non-primitive
+//primitive
+let a='hello';
+let b=a;
+console.log(a,b)
+a='gello';
+console.log(a,b)
+
+//non-primitive
+const x={job:'web developer'}
+const y=x;
+console.log(x,y)
+x.job='back end developer'
+console.log(x,y)
